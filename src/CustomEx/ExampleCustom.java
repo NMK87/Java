6346696b174e5d8e298d2c61f75656a43a7873e8
NMK87 <nehaprabhakar0801@gmail.com>
2023-01-07 22:57:43 +0530

    Custom commit

---
+++ src/CustomEx/ExampleCustom.java
@@ -0,0 +1,33 @@
+package CustomEx;
+
+class FailedStudentsException extends Exception{
+	
+	FailedStudentsException(String msg){
+		System.out.println(msg);
+	}
+}
+
+public class ExampleCustom {
+
+	public static void main(String[] args) {
+		
+	try {
+		School(8);
+	}
+	catch(Exception e) {
+		System.out.println(e);
+	}
+}
+
+
+public static void School(int marks) throws FailedStudentsException {
+	
+	if(marks<=35) {
+		 throw new FailedStudentsException("Below 35 marks will get failed");
+	}
+	
+	else {
+		System.out.println("passed");
+		}
+	}
+}
